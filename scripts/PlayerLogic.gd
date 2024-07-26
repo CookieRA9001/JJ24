29a57bc4a4672e77030d914a0b738e25c579e9a7
RalfsAizsils <ralfs.aizsils@edu.rtu.lv>
2024-07-26 14:45:51 +0300

    level ups

--- scripts/PlayerLogic.gd
+++ scripts/PlayerLogic.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-const SPEED: float = 300.0
 @onready var camera_2d = $Camera2D
 @onready var health_bar = $HealthBar
+@onready var xp_bar = $XPBar
 @onready var aim = $Aim
 @onready var body = $Body
 @onready var shoot_time = $Aim/ShootTime
@@ -11,6 +11,9 @@ const SPEED: float = 300.0
 @export var health := maxHealth
 @export var damage := 1.0
 @export var xp := 0.0
+@export var level := 1
+@export var nextLevel := 10.0
+@export var speed: float = 300.0
 
 func _ready():
 	health = 1
@@ -20,10 +23,10 @@ func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("Left", "Right"),Input.get_axis("Up", "Down")).normalized()
 	
 	if direction:
-		velocity = direction * SPEED
+		velocity = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
 		
 	if velocity.x > 0:
 		body.scale.x = -1
@@ -41,6 +44,13 @@ func _process(delta):
 		bullet.init(position, damage, (get_global_mouse_position()-global_position).normalized(), 40000)
 		shoot_time.start()
 	
+	# exp
+	xp_bar.scale.x = min(xp/nextLevel * 4,4)
+	# level up
+	if xp >= nextLevel:
+		levelUp()
+		
+	
 func updateHealth(delta):
 	health += delta
 	health_bar.scale.x = health/maxHealth * 4
@@ -50,3 +60,15 @@ func updateHealth(delta):
 
 func _on_exp_collector_body_entered(body):
 	body.Target = self
+	
+func levelUp():
+	xp -= nextLevel
+	level += 1
+	nextLevel *= 1.25
+	
+	damage += 0.1
+	maxHealth += 1
+	health = maxHealth
+	speed += 10
+	shoot_time.wait_time = 0.5 / (0.98 + 0.02*level)
+	
