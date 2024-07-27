a7dfcc5e67946f9551c4ae908a8b4619043288d8
RalfsAizsils <ralfs.aizsils@edu.rtu.lv>
2024-07-27 20:05:47 +0300

    .

--- scripts/GameManger.gd
+++ scripts/GameManger.gd
@@ -12,28 +12,128 @@ extends Node2D
 @export var Waves = [
 	[
 		{
-			"time": 10,
+			"time": 6, # 6
 			"enemies": [5, "virus"]
 		},
 		{
-			"time": 10,
+			"time": 9, #15
 			"enemies": [10, "virus"]
 		},
 		{
-			"time": 20,
+			"time": 11, #26
 			"enemies": [10, "virus", 1, "trojen"]
 		},
 		{
-			"time": 30,
-			"enemies": [20, "virus", 3, "trojen"]
+			"time": 10, #36
+			"enemies": [10, "virus2", 1, "folder"]
 		},
 		{
-			"time": 10,
-			"enemies": [30, "virus"]
+			"time": 14, #50
+			"enemies": [20, "virus", 10, "virus2"]
 		},
 		{
-			"time": 0,
-			"enemies": [1, "end", 20, "trojen"]
+			"time": 30, # 1 20
+			"enemies": [10, "ads", 10, "virus", 5, "trojen"]
+		},
+		{
+			"time": 10, # 1 30
+			"enemies": [10, "ads", 10, "virus", 1, "folder"]
+		},
+		{
+			"time": 30, # 2 00
+			"enemies": [10, "spike", 2, "folder"]
+		},
+		{
+			"time": 15, # 2 15
+			"enemies": [20, "trojen", 1, "folder2"]
+		},
+		{
+			"time": 20, # 2 35
+			"enemies": [5, "virus3", 20, "virus", 10, "spike"]
+		},
+		{
+			"time": 10, # 2 45
+			"enemies": [3, "trojen2", 10, "virus2"]
+		},
+		{
+			"time": 30, # 3 15
+			"enemies": [5, "ads2", 20, "virus2", 10, "trojen", 1, "folder2"]
+		},
+		{
+			"time": 30, # 3 45
+			"enemies": [10, "ads2", 10, "virus3", 15, "trojen", 2, "folder2"]
+		},
+		{
+			"time": 20, # 4 05
+			"enemies": [20, "ads", 20, "virus", 10, "trojen", 4, "folder"]
+		},
+		{
+			"time": 10, # 4 15
+			"enemies": [5, "virus4", 5, "trojen2", 20, "ads2"]
+		},
+		{
+			"time": 20, # 4 35
+			"enemies": [20, "virus4"]
+		},
+		{
+			"time": 30, # 5 05
+			"enemies": [30, "virus4"]
+		},
+		{
+			"time": 20, # 5 25
+			"enemies": [10, "spike2", 20, "spike", 3, "folder2"]
+		},
+		{
+			"time": 20, # 5 45
+			"enemies": [10, "trojen2"]
+		} ,
+		{
+			"time": 25, # 6 10
+			"enemies": [10, "spike2", 20, "virus4", 20, "ads2"]
+		} ,
+		{
+			"time": 20, # 6 30
+			"enemies": [30, "spike2"]
+		} ,
+		{
+			"time": 30, # 7 00
+			"enemies": [30, "virus4"]
+		},
+		{
+			"time": 20, # 7 20
+			"enemies": [20, "trojen2"]
+		},
+		{
+			"time": 20, # 7 40
+			"enemies": [5, "spike2", 1, "folder3"]
+		},
+		{
+			"time": 20, # 8 00
+			"enemies": [10, "spike2", 3, "folder2"]
+		},
+		{
+			"time": 20, # 8 00
+			"enemies": [10, "virus4", 10, "ads2", 10, "trojen2", 20, "virus3"]
+		},
+		{
+			"time": 30, # 8 30
+			"enemies": [15, "virus4", 25, "virus3"]
+		},
+		{
+			"time": 30, # 9 00
+			"enemies": [10, "virus5"]
+		},
+		{
+			"time": 20, # 9 20
+			"enemies": [10, "virus5", 10, "trojen2"]
+		},
+		{
+			"time": 20, # 9 40
+			"enemies": [20, "virus5", 10, "spike2", 10, "ads2", 3, "folder3"]
+		},
+		{
+			"time": 20, # 10 00
+			"enemies": [20, "virus5", 20, "trojen3"]
 		}
 	]
 ]
@@ -62,15 +162,42 @@ func _on_spawn_timer_timeout():
 		match enemy:
 			"virus":
 				enemy = load("res://objects/enemy.tscn")
+			"virus2":
+				enemy = load("res://objects/enemy_2.tscn")
+			"virus3":
+				enemy = load("res://objects/enemy_3.tscn")
+			"virus4":
+				enemy = load("res://objects/enemy_4.tscn")
+			"virus5":
+				enemy = load("res://objects/enemy_5.tscn")
+			"ads":
+				enemy = load("res://objects/enemy_add.tscn")
+			"ads2":
+				enemy = load("res://objects/enemy_add_2.tscn")
+			"spike":
+				enemy = load("res://objects/enemy_worm.tscn")
+			"spike2":
+				enemy = load("res://objects/enemy_worm_2.tscn")
 			"trojen":
 				enemy = load("res://objects/enemy_trojan.tscn")
+			"trojen2":
+				enemy = load("res://objects/enemy_trojan_2.tscn")
+			"trojen3":
+				enemy = load("res://objects/enemy_trojan_3.tscn")
+			"folder":
+				enemy = load("res://objects/folder.tscn")
+			"folder2":
+				enemy = load("res://objects/folder2.tscn")
+			"folder3":
+				enemy = load("res://objects/folder3.tscn")
 			"end":
 				enemy = load("res://objects/enemy.tscn") # :P
 		
 		for j in enemyCount:
 			var mob = enemy.instantiate()
 			mob.global_position = get_random_position()
-			mob.GM = self
+			if "GM" in mob:
+				mob.GM = self
 			add_child(mob)
 	
 
